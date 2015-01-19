a86211c3abe75ca95d80753362800b207fa8ceec
Suraj Yerramilli <surajy123@gmail.com>
2015-01-19 17:55:24 +0530

    Added support to read from dataframe objects and excel files

--- R/readData.R
+++ R/readData.R
@@ -1,8 +1,6 @@
-# Reading from table-formatted files
-read.idframe(file,header=TRUE,sep=",",ninputs=1,
-             type=c("time","freq")[1],Ts = 1,freqData=FALSE...){
-  # Read from file (default: csv file)
-  dat <- read.table(file=file,header=header,sep=sep,...)
+# Reading data from dataframes
+readData.idframe <- function(data,ninputs=1,type=c("time","freq")[1],
+                 Ts = 1,freqData=FALSE){
   
   if((type=="freq") && (freqData)){
     
@@ -20,4 +18,32 @@ read.idframe(file,header=TRUE,sep=",",ninputs=1,
     
     out <- idframe(output=outputs,input=inputs,type=type,Ts=Ts)
   }
+  
+  return(out)
+}
+
+# Reading from table-formatted files
+read.idframe <- function(file,header=TRUE,sep=",",ninputs=1,
+             type=c("time","freq")[1],Ts = 1,freqData=FALSE,...){
+  
+  # Read from file (default: csv file)
+  dat <- read.table(file=file,header=header,sep=sep,...)
+  
+  # read from dataframe and return idframe object
+  out <- readData.idframe(data,ninputs=ninputs,type=type,Ts = Ts,freqData=freqData)
+  return(out)
+}
+
+# Reading from excel files
+readxls.idframe <- function(file,sheetName,header=TRUE,ninputs=1,
+                type=c("time","freq")[1],Ts = 1,freqData=FALSE,...){
+  
+  require(xlsx)
+  
+  # Read from file (default: csv file)
+  dat <- read.xlsx2(file=file,sheetName=sheetName,header=header,...)
+  
+  # read from dataframe and return idframe object
+  out <- readData.idframe(data,ninputs=ninputs,type=type,Ts = Ts,freqData=freqData)
+  return(out)
 }
