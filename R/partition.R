f9b0b6607be23df7929acab838767a62b502bfbd
Suraj Yerramilli <surajy123@gmail.com>
2015-02-07 00:59:33 +0530

    changed argument names

--- R/partition.R
+++ R/partition.R
@@ -1,19 +1,19 @@
-#' Subset an idframe object
+#' Subset an idframe data
 #' 
-#' Subsetting method for objects of class \code{idframe}
+#' Subsetting method for datas of class \code{idframe}
 #' 
-#' @param object an object of class \code{idframe}
+#' @param data an object of class \code{idframe}
 #' @param indices the indices that need to be subsetted
 #' @export
-dataSlice <- function(object,indices){
+dataSlice <- function(data,indices){
   # check if the class is correct
-  if(class(object)!='idframe')
-    stop("Not an idframe object")
+  if(class(data)!='idframe')
+    stop("Not an idframe data")
   
-  if(!all(indices %in% seq(to=dim(object$output)[1],by=1)))
+  if(!all(indices %in% seq(to=dim(data$output)[1],by=1)))
     stop("Invalid indices")
   
-  trim <- object
+  trim <- data
   trim$output <- trim$output[indices,,drop=F]
   trim$input <- trim$input[indices,,drop=F]
   
@@ -32,9 +32,9 @@ dataSlice <- function(object,indices){
 #' The function splits the data into training and validation sets and returns them bundled
 #' as a list. The size of the sets are determined by the parameter \code{p}.
 #' 
-#' @param object an object of class \code{idframe}
+#' @param data an object of class \code{idframe}
 #' @param p the percentage of the data that goes to training (Default : \code{0.6})
-#' @return list containing estimation and validation idframe objects
+#' @return list containing estimation and validation idframe datas
 #' 
 #' @examples
 #' data(cstr)
@@ -43,18 +43,18 @@ dataSlice <- function(object,indices){
 #' test <- splitList$validation # testing set
 #' 
 #' @export
-dataPartition <- function(object,p=0.6){
+dataPartition <- function(data,p=0.6){
   # check if the class is correct
-  if(class(object)!='idframe')
-    stop("Not an idframe object")
+  if(class(data)!='idframe')
+    stop("Not an idframe data")
   
-  index <- seq_along(object$output[,1])
+  index <- seq_along(data$output[,1])
   
   trainIndex <- index[1:round(p*length(index))]
   testIndex <- index[!(index %in% trainIndex)]
   
-  train <- dataSlice(object,trainIndex)
-  test <- dataSlice(object,testIndex)
+  train <- dataSlice(data,trainIndex)
+  test <- dataSlice(data,testIndex)
   
   return(list(estimation=train,validation=test))
 }
