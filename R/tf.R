aa8d6a9fa5c020a2988a370150624f403121c939
Suraj Yerramilli <surajy123@gmail.com>
2015-03-21 23:34:11 +0530

    added a basic transfer function object

--- R/tf.R
+++ R/tf.R
@@ -1,6 +1,8 @@
 # S3 class for defining trasnfer functions
 tf <- function(num=c(1),den=c(1),Ts=1){
-  
+  out <- list(num=num,den=den,Ts=Ts)
+  class(out) <- "tf"
+  return(out)
 }
 
 # Display the Transfer Function
