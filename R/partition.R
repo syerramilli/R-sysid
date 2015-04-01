2f168a71af236331a469b7dd794aca70d7ae6022
Suraj Yerramilli <surajy123@gmail.com>
2015-04-01 23:12:32 +0530

    Updated the documentation for the dataSlice function

--- R/partition.R
+++ R/partition.R
@@ -1,9 +1,29 @@
 #' Subset an idframe data
 #' 
-#' Subsetting method for objects of class \code{idframe}
+#' \code{dataSlice} is a Subsetting method for objects of class \code{idframe}. It 
+#' extracts the subset of the object \code{data} observed between indices \code{start}
+#' and \code{end}.  If a frequency is specified, the series is then re-sampled at the 
+#' new frequency.
 #' 
 #' @param data an object of class \code{idframe}
-#' @param start the start
+#' @param start the start index
+#' @param end the end index
+#' @param freq the new sampling frequency
+#' 
+#' @details
+#' The dataSlice function extends the \code{\link[stats]{window}} function for idframe
+#' objects
+#' 
+#' @return an idframe object
+#' 
+#' @examples
+#' data(cstr)
+#' cstrsub <- dataSlice(cstr,start=200,end=400) # extract between indices 200 and 400
+#' cstrTrain <- dataSlice(cstr,end=4500) # extract upto index 4500
+#' cstrTest <- dataSlice(cstr,start=6501) # extract from index 6501 till the end
+#' cstr_new <- dataSlice(cstr,freq=3) # resample data at thrice the frequency  
+#' 
+#' @seealso \code{\link[stats]{window}}
 #' @export
 dataSlice <- function(data,start=NULL,end=NULL,freq=NULL){
   # check if the class is correct
