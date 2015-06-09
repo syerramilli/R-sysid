8229064a7e915ddac37dbcf3b8055696d0762114
bnarsim <narasimhan1993@gmail.com>
2015-06-09 07:35:45 +0530

    Code for RGS. Even in default case, band needs to be specified

---
+++ R/rgs.R
@@ -0,0 +1,23 @@
+idin.rgs <- function(n,band,var){
+  # Function to generate a random binary 
+  # signal of given frequency band and levels
+  require(signal)
+  uk1 <- rnorm(n,mean = 0,sd = 1)
+  uk <- rep(0,n)
+  for(i in 1:n){
+    #Checking for zeros
+    
+    if(uk1[i] == 0){
+      uk1[i] <- rnorm(1,mean = 0,sd = 1)
+    }
+  }
+  # Getting the filter coefficients
+  bfilt <- butter(8,c(band[1],band[2]),type = "pass",plane = "z")
+ 
+  # Filtering the signal
+  ukf <- filter(bfilt,uk)
+  
+  # Adjusting for required variance
+  uk <- sqrt(var)*ukf
+  return(uk)
+}
