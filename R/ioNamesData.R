66e204ae6e6720c80de116c56895a75ea6674949
Suraj Yerramilli <surajy123@gmail.com>
2015-08-21 15:58:02 +0530

    Initialized functions for extracting the names and data from  idframe objects

---
+++ R/ioNamesData.R
@@ -0,0 +1,11 @@
+#' @export
+nInputSeries <- function(data) ncol(data$input)
+
+#' @export
+nOutputSeries <- function(data) ncol(data$output)
+
+#' @export
+inputData <- function(x){}
+
+#' @export
+outPutData <- function(x){}
