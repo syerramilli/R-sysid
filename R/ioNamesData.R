11fb3a56560e83f571f59266e20dbca331deddd1
Suraj Yerramilli <surajy123@gmail.com>
2015-08-21 16:33:52 +0530

    Added auxillary functions to NAMESPACE and modified them

--- R/ioNamesData.R
+++ R/ioNamesData.R
@@ -1,3 +1,5 @@
+require(tframe)
+
 #' @export
 nInputSeries <- function(data) ncol(data$input)
 
@@ -5,7 +7,57 @@ nInputSeries <- function(data) ncol(data$input)
 nOutputSeries <- function(data) ncol(data$output)
 
 #' @export
-inputData <- function(x){}
+inputData <- function(x,series) UseMethod("inputData")
 
 #' @export
-outPutData <- function(x){}
+inputData.default <- function(x,series=NULL){
+  print("Not defined for this datatype")
+}
+
+#' @export
+inputData.idframe <- function(x,series=seq(nInputSeries(x))){
+  if (is.null(x$input))
+    NULL
+  else selectSeries(x$input,series=series)
+}
+
+#' @export
+inputNames <- function(x) UseMethod("inputNames")
+
+#' @export
+inputNames.default <- function(x){
+  print("Not defined for this datatype")
+}
+
+#' @export
+inputNames.idframe <- function(x){
+  seriesNames(inputData(x))
+}
+
+#' @export
+outputData <- function(x,series) UseMethod("outputData")
+
+#' @export
+outputData.default <- function(x,series=NULL){
+  print("Not defined for this datatype")
+}
+
+#' @export
+outputData.idframe <- function(x,series=seq(nOutputSeries(x))){
+  if (is.null(x$output))
+    NULL
+  else selectSeries(x$output,series=series)
+}
+
+#' @export
+outputNames <- function(x) UseMethod("outputNames")
+
+#' @export
+outputNames.default <- function(x){
+  print("Not defined for this datatype")
+}
+
+#' @export
+outputNames.idframe <- function(x){
+  seriesNames(outputData(x))
+}
