74e4db50e71c1dc588c78e7589d9da7958a9be7a
Suraj Yerramilli <surajy123@gmail.com>
2016-01-02 17:44:27 +0530

    removed redundant require statements

--- R/idinput.R
+++ R/idinput.R
@@ -21,6 +21,7 @@
 #' For 'rgs', minu=mean value of signal minus one standard deviation and maxu=mean value of signal plus one standard deviation
 #' 
 #' Default value is levels=c(-1,1)
+#' 
 #' @export
 idinput<-function(n,type='rgs',band=c(0,1),levels=c(-1,1)){
   if(type=="rbs"){
@@ -33,24 +34,22 @@ idinput<-function(n,type='rgs',band=c(0,1),levels=c(-1,1)){
 }
 
 gen.rgs<-function(n,band,levels){
-  require(signal)
   mu<-(levels[1]+levels[2])/2
   sigma<-(levels[2]-levels[1])/2
   v<-rnorm(n,mu,sigma)
   v<-sapply(v, function(x) {if(x==0) rnorm(1) else x})
-  gfilt<-butter(8,band,type ='pass',plane ='z')
-  v1<-filter(gfilt,v)
+  gfilt<-signal::butter(8,band,type ='pass',plane ='z')
+  v1<-signal::filter(gfilt,v)
   return(v1)
 }
 
 gen.rbs<-function(n,band,levels){
-  require(signal)
   v<-rnorm(n)
   
   v<-sapply(v, function(x) {if(x==0) rnorm(1) else x}, simplify = 'vector')
   #if we do not specify else case, it assigns it as NULL
-  bfilt<-butter(8,band,type = 'pass',plane = 'z')
-  v1<-filter(bfilt,v)
+  bfilt<-signal::butter(8,band,type = 'pass',plane = 'z')
+  v1<-signal::filter(bfilt,v)
   v1<-sapply(v1, function(x) {ifelse(x>0 , levels[2] , levels[1]) })
   return(v1)
 }
