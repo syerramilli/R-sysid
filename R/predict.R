c064ec62ec2c7cf3d761addc8e0495ab82b7ed19
Suraj Yerramilli <surajy123@gmail.com>
2016-01-04 18:31:50 +0530

    added one-step ahead predictions

--- R/predict.R
+++ R/predict.R
@@ -1,14 +1,20 @@
 #' @export
 predict.idpoly <- function(x,data,nahead=1){
-  y <- outputData(z); u<- inputData(z)
+  y <- outputData(x); u<- inputData(x)
   G <- signal::Arma(b=c(rep(0,x$ioDelay),x$B),
                     a= as.numeric(polynom::polynomial(x$A)*
                                     polynom::polynomial(x$F1)))
+  det_sys <- signal::filter(G,u)
   if(x$type=="oe" || nahead==Inf){
-    ypred <- signal::filter(G,u)
+    ypred <- det_sys
   } else{
     Hden <- as.numeric(polynom::polynomial(x$A)*polynom::polynomial(x$D))
-    Hinv <- signal::Arma(b=Hden,a=x$C) 
+    Hinv <- signal::Arma(b=Hden,a=x$C)
+    filtered <- signal::filter(Hinv,y-det_sys)
+    if(nahead!=1){
+      
+    }
+    ypred <- y[k] - filtered
   }
   
   return(ypred)
@@ -29,4 +35,8 @@ predict.estpoly <- function(x,newdata=NULL,nahead=1){
     }
     predict(model,z,nahead)
   } 
+}
+
+polyinv <- function(x,k){
+  gamma <- 1/Re(polyroot(x))
 }
