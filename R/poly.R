36c9ca307cc178643f1aa6233e89a838bb3526b2
Suraj Yerramilli <surajy123@gmail.com>
2016-02-27 14:20:15 +0530

    doc update

--- R/poly.R
+++ R/poly.R
@@ -24,7 +24,7 @@
 #' mod_oe <- idpoly(B=c(0.6,-0.2),F1=c(1,-0.5),ioDelay = 2,Ts=0.1,
 #' noiseVar = 0.1)
 #' 
-#' # define box-jenkins model
+#' # define box-jenkins model with unit variance
 #' B <- c(0.6,-0.2)
 #' C <- c(1,-0.3)
 #' D <- c(1,1.5,0.7)
