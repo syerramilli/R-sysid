9a894b78aea2bfeb77042fd3f9e4598aee6223b2
Suraj Yerramilli <surajy123@gmail.com>
2016-03-18 18:16:03 +0530

    fixing integrated noise part

--- R/estpoly.R
+++ R/estpoly.R
@@ -163,11 +163,12 @@ residplot <- function(model,newdata=NULL){
 #' 
 #' @export
 arx <- function(x,order=c(1,1,1),lambda=0.1,intNoise=FALSE){
-  y <- outputData(x); u <- inputData(x); N <- dim(y)[1]
+  y <- outputData(x); u <- inputData(x)
   if(intNoise){
-    y <- apply(y,2,integfilter)
-    u <- apply(u,2,integfilter)
+    y <- apply(y,2,diff)
+    u <- apply(u,2,diff)
   }
+  N <- dim(y)[1]
   na <- order[1];nb <- order[2]; nk <- order[3]
   nb1 <- nb+nk-1 ; n <- max(na,nb1); df <- N-na-nb
   
