c405f74cc52361b4157bc46041e2182b07d00359
Suraj Yerramilli <surajy123@gmail.com>
2017-01-06 13:57:11 -0600

    minor changes

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,4 @@
-#' @import stats
+#' @importFrom stats predict resid arima ts start ar acf ccf coef fitted lm mvfft rnorm window
 #' @importFrom graphics par plot
 #' @importFrom utils read.table tail
 #' @import grid
