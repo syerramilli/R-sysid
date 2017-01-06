2ea98243bd2f7d3732cc93054baef9ec5638959b
Suraj Yerramilli <surajy123@gmail.com>
2017-01-06 11:45:35 -0600

    adding imported functions

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,5 @@
-#' @importFrom stats predict resid arima ts start ar ccf coef fitted lm mvfft rnorm window
+#' @importFrom stats predict resid arima ts start ar acf ccf coef fitted lm mvfft rnorm window
 #' @importFrom graphics par plot
 #' @importFrom utils read.table tail
+#' @import grid
 NULL
